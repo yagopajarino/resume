a56d96cb543932beff980930e04bb3456feaf1e6
Yago Pajariño <yago977@hotmail.com>
2022-05-30 21:23:14 -0300

    Adds typing writer effect

--- src/components/welcomeSection.js
+++ src/components/welcomeSection.js
@@ -1,7 +1,27 @@
+import { useState, useEffect } from "react";
+import typing from "../templates/typing";
+
 export default function WelcomeSection() {
+  const [texto, setTexto] = useState("");
+
+  useEffect(() => {
+    let i = 0;
+    let s = "hola que tal como le va?";
+
+    const intervalo = setInterval(() => {
+      if (i < s.length - 1) {
+        setTexto(s.slice(0, i + 1));
+        i++;
+      } else {
+        setTexto(s);
+        clearInterval(intervalo);
+      }
+    }, 100);
+  }, []);
+
   return (
     <div className="welcomeSection">
-      <h1>HELLO</h1>
+      <h1>{texto}</h1>
     </div>
   );
 }
