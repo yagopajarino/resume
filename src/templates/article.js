8697307519282f195f59e20e08b840fc9287b295
Yago Pajariño <yago977@hotmail.com>
2022-06-02 12:15:04 -0300

    Adds article and main section components

---
+++ src/templates/article.js
@@ -0,0 +1,11 @@
+import SectionHeader from "./sectionHeader";
+import SectionMain from "./sectionMain";
+
+export default function Article(props) {
+  return (
+    <article className="main" id="routerContainer">
+      <SectionHeader title={props.title} subtitle={props.subtitle} />
+      <SectionMain content={props.content} />
+    </article>
+  );
+}
