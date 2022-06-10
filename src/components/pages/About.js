20b54cfe7d859885cb58f128f58bd2c119f1e149
Yago Pajariño <yago977@hotmail.com>
2022-06-10 13:10:15 -0300

    Adds state to About.js

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,8 +1,12 @@
 import Article from "../../templates/article";
 import pagesData from "../../data/pagesData.json";
+import { useState } from "react";
 
 export default function About(props) {
-  let data = pagesData.filter((item) => item.title.toUpperCase() == "ABOUT")[0];
+  const [data, setData] = useState(
+    pagesData.filter((item) => item.title.toUpperCase() == "ABOUT")[0]
+  );
+
   return (
     <Article title={data.title} subtitle={data.subtitle} fileName="about" />
   );
