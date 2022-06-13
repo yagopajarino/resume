c357807177bf8525a791b03d298f30238eb77f04
Yago Pajariño <yago977@hotmail.com>
2022-06-13 17:12:32 -0300

    Adds projects

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,19 +1,54 @@
-import Article from "../../templates/article";
+import SectionHeader from "../../templates/sectionHeader";
 import pagesData from "../../data/pagesData.json";
+import projectsData from "../../data/projects.json";
+import img_mmanager from "../../static/mmanager.png";
+import img_wengine from "../../static/wengine.png";
+import img_top from "../../static/TOP.png";
+import img_uba from "../../static/dc-uba.png";
+import { useState } from "react";
 
 export default function Projects() {
-  let data = pagesData.filter(
-    (item) => item.title.toUpperCase() == "PROJECTS"
-  )[0];
+  const [data, setData] = useState(
+    pagesData.filter((item) => item.title.toUpperCase() == "PROJECTS")[0]
+  );
+  const [pros, setPros] = useState(projectsData);
 
   function effects() {}
 
+  const imagenes = [img_wengine, img_top, img_mmanager, img_uba];
+  pros.forEach((element) => {
+    for (let index = 0; index < pros.length; index++) {
+      pros[index].img = imagenes[index];
+    }
+  });
+
+  const proyectos = pros.map((p) => {
+    return (
+      <section key={p.id}>
+        <h2>
+          {p.title} | <span>{p.date}</span>
+        </h2>
+        <div>
+          <a href={p.link} target="_blank">
+            <img src={p.img} />
+          </a>
+          <p>{p.description}</p>
+        </div>
+      </section>
+    );
+  });
+
+  console.log(pros);
   return (
-    <Article
-      title={data.title}
-      subtitle={data.subtitle}
-      fileName="projects"
-      effects={effects}
-    />
+    // <Article
+    //   title={data.title}
+    //   subtitle={data.subtitle}
+    //   fileName="projects"
+    //   effects={effects}
+    // />
+    <article className="main" id="routerContainer">
+      <SectionHeader title={data.title} subtitle={data.subtitle} />
+      <main className="project-body">{proyectos}</main>
+    </article>
   );
 }
