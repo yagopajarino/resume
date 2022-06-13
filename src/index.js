8d53110b6571d9084373a3c07c0e603475c8131d
Yago Pajariño <yago977@hotmail.com>
2022-06-13 02:15:38 -0300

    Adds refreshing to reactRouter

--- src/index.js
+++ src/index.js
@@ -14,29 +14,32 @@ import "./style/markdown.css";
 import "./style/nav.css";
 import "./style/info-container.css";
 import "./style/article-header.css";
+import React from "react";
 
 let icon = document.querySelector("#icon");
 icon.href = logo;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route path="/" element={<Welcome />} />
-        <Route path="about" element={<About />} />
-        <Route path="contact" element={<Contact />} />
-        <Route path="Projects" element={<Projects />} />
-        <Route path="resume" element={<Resume />} />
-        <Route path="stats" element={<Stats />} />
-        <Route
-          path="*"
-          element={
-            <main style={{ padding: "1rem" }}>
-              <p>There's nothing here!</p>
-            </main>
-          }
-        />
-      </Route>
-    </Routes>
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="/" element={<Welcome />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="Projects" element={<Projects />} />
+          <Route path="resume" element={<Resume />} />
+          <Route path="stats" element={<Stats />} />
+          <Route
+            path="*"
+            element={
+              <main style={{ padding: "1rem" }}>
+                <p>There's nothing here!</p>
+              </main>
+            }
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 );
