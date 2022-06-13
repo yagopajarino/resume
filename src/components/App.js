f7f19e879b0f35c74535efdeb29577d833f24c49
Yago Pajariño <yago977@hotmail.com>
2022-06-13 01:12:17 -0300

    Adds arrowToTop component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import NavBar from "./navBar";
 import MainFrame from "./mainFrame";
 import React from "react";
+import ArrowUp from "./arrowToTop";
 
 export default function App() {
   return (
     <div>
       <NavBar />
       <MainFrame />
+      <ArrowUp />
     </div>
   );
 }
---
+++ src/components/arrowToTop.js
@@ -0,0 +1,42 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { useEffect } from "react";
+
+library.add(fas);
+
+export default function ArrowToTop() {
+  useEffect(() => {
+    const mybutton = document.querySelector("#arrowContainer");
+
+    function scrollFunction() {
+      const pxDelta = document.querySelector("nav").offsetHeight - 1;
+      console.log(pxDelta);
+      if (
+        document.body.scrollTop > pxDelta ||
+        document.documentElement.scrollTop > pxDelta
+      ) {
+        mybutton.style.bottom = "30px";
+      } else {
+        mybutton.style.bottom = "-100px";
+      }
+    }
+
+    window.onscroll = () => {
+      scrollFunction();
+    };
+
+    function topFunction() {
+      document.body.scrollTop = 0; // For Safari
+      document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    }
+
+    mybutton.addEventListener("click", topFunction);
+  }, []);
+
+  return (
+    <div id="arrowContainer">
+      <FontAwesomeIcon icon="fa-solid fa-arrow-up" size="3x" />
+    </div>
+  );
+}
