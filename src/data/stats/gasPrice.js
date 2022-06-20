50b8f07f39aff3641d7f9f18b03e3813a0319f57
Yago Pajariño <yago977@hotmail.com>
2022-06-20 19:09:49 -0300

    Add USD conversion and map link

--- src/data/stats/gasPrice.js
+++ src/data/stats/gasPrice.js
@@ -1,10 +1,14 @@
-export default async function getGasPrice(state, setState) {
+export default async function getGasPrice(setPrice, setMap) {
   const url = "https://apis.is/petrol";
   const data = await fetch(url).then((response) => response.json());
   const results = data.results;
   let k = Math.floor(Math.random() * results.length);
   const station = results[k];
-  if (state == "Loading...") {
-    setState(station.bensin95);
-  }
+  const factor = 0.008;
+  const lat = station.geo.lat;
+  const lon = station.geo.lon;
+  const link = `http://www.google.com/maps/place/${lat},${lon}`;
+  setPrice((station.bensin95 * factor).toFixed(4));
+  setMap(link);
+  console.log(link);
 }
