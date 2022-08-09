4271d2614c0b1cd0b83787662ea82314df6e3564
Yago Pajariño <yagopajarino@outlook.com>
2022-08-09 20:15:50 -0300

    Fix getAge function

--- src/data/stats/getAge.js
+++ src/data/stats/getAge.js
@@ -1,9 +1,24 @@
 export default function getAge(state, setState) {
+  const getDateCumpleaños = (año) => {
+    return Date.parse(`Aug 11, ${año} 10:55:00 GMT-03`);
+  };
+
   setInterval(() => {
-    let now = new Date();
-    let nacimiento = Date.parse("Aug 11, 1997 10:40:00 GMT-03");
-    let ageDiff = now - nacimiento;
-    let milisec_per_year = 365 * 24 * 60 * 60 * 1000;
-    setState((ageDiff / milisec_per_year).toFixed(11));
+    const now = new Date();
+    const añoActual = now.getFullYear();
+    const cumpleaños = getDateCumpleaños(añoActual);
+    let añosCumplidos, milisec, total;
+    if (now >= cumpleaños) {
+      añosCumplidos = añoActual;
+      milisec = now - cumpleaños;
+      total = getDateCumpleaños(añoActual + 1) - cumpleaños;
+    } else {
+      let año_pasado = getDateCumpleaños(añoActual - 1);
+      añosCumplidos = añoActual - 1;
+      milisec = now - año_pasado;
+      total = cumpleaños - año_pasado;
+    }
+
+    setState((añosCumplidos - 1997 + milisec / total).toFixed(11));
   }, 10);
 }
