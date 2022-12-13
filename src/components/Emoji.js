57bb9953588654feff4055e56c18d5ae2e5add39
Yago Pajariño <yagopajarino@outlook.com>
2022-12-13 15:36:14 -0300

    Update Emoji.js component

--- src/components/Emoji.js
+++ src/components/Emoji.js
@@ -1,12 +1,23 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import api from "../api/api";
 
 export default function Emoji(props) {
   const [text, setText] = useState(props.symbol);
   const [isEmoji, setIsEmoji] = useState(true);
 
+  const handleClick = (e) => {
+    const code = e.target.id;
+    api.postCountry(code);
+    props.setIsLoading(true);
+    setTimeout(() => {
+      props.setShowScores(true);
+    }, 2000);
+  };
+
   return (
     <div
       className={`container ${isEmoji ? "emoji" : ""}`}
+      id={props.code}
       onMouseEnter={() => {
         setText(props.name);
         setIsEmoji(false);
@@ -15,6 +26,7 @@ export default function Emoji(props) {
         setText(props.symbol);
         setIsEmoji(true);
       }}
+      onClick={handleClick}
     >
       {text}
     </div>
