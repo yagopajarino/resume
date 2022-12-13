30414bad6fdf3085a4e5914276c8ed3924285c2e
Yago Pajariño <yagopajarino@outlook.com>
2022-12-13 15:30:17 -0300

    Add Scoreboard component

---
+++ src/components/Scoreboard.js
@@ -0,0 +1,37 @@
+import react, { useEffect, useState } from "react";
+import api from "../api/api";
+import emojiFlags from "emoji-flags";
+
+export default function Scoreboard() {
+  const [scores, setScores] = useState([]);
+
+  useEffect(() => {
+    const callScores = async () => {
+      const data = await api.getScores();
+      setScores(data);
+    };
+    callScores();
+  }, []);
+
+  return (
+    <table>
+      <thead>
+        <th>Flag</th>
+        <th>Country</th>
+        <th>Count</th>
+      </thead>
+      <tbody>
+        {scores.map((i) => {
+          const country = emojiFlags[i.code];
+          return (
+            <tr>
+              <td className="flag">{country.emoji}</td>
+              <td>{country.name}</td>
+              <td>{i.count}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
