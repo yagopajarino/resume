7c7e5bc18c2c5194f6507dbfff219e890cfa78d8
Yago Pajariño <yagopajarino@outlook.com>
2022-12-13 15:31:41 -0300

    Add Loading component

---
+++ src/components/Loading.js
@@ -0,0 +1,11 @@
+import Spinner from "./Spinner";
+
+export default function Loading() {
+  return (
+    <div className="loading">
+      <div className="container">
+        <Spinner />
+      </div>
+    </div>
+  );
+}
