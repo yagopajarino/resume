59c56624cf9c8cb26beed2f3ca3aaf6fea307293
Yago Pajariño <yagopajarino@outlook.com>
2022-12-14 13:12:20 -0300

    Add api caller class

---
+++ src/api/api.js
@@ -0,0 +1,32 @@
+const getScores = async () => {
+  const url = `${process.env.REACT_APP_API}/paises`;
+  const data = await fetch(url).then((r) => r.json());
+  data.sort((a, b) => b.count - a.count);
+  return data;
+};
+
+const postCountry = async (code) => {
+  const url = `${process.env.REACT_APP_API}/paises/update`;
+  const data = {
+    code,
+  };
+  const status = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: { "Content-Type": "application/json" },
+  }).then((r) => r.json());
+  return status;
+};
+
+const ping = () => {
+  const url = `${process.env.REACT_APP_API}`;
+  fetch(url);
+};
+
+const api = {
+  ping,
+  getScores,
+  postCountry,
+};
+
+export default api;
